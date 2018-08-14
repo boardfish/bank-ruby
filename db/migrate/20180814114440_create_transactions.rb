90c4eb8fcfeaf0f9e5f577559991b7663f455750
Simon Fish <sfish@ukcloud.com>
2018-08-14 12:43:14 +0100

    Initial commit

---
+++ db/migrate/20180814114440_create_transactions.rb
@@ -0,0 +1,18 @@
+class CreateTransactions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :transactions do |t|
+      t.decimal :amount
+      t.string :decline_reason
+      t.boolean :is_load
+      t.datetime :settled
+      t.string :category
+      t.string :merchant
+      t.datetime :created
+      t.string :currency
+      t.string :description
+      t.string :notes
+
+      t.timestamps
+    end
+  end
+end
