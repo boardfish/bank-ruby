5b008ce004e002c3056a1cfd4a3f05600021ed9f
Simon Fish <sfish@ukcloud.com>
2018-08-16 17:00:24 +0100

    Change root to transactions#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'transactions#index'
   resources :transactions
   post '/monzo_webhook_add', to: 'transactions#monzo_webhook_add'
   get '/summary', to: 'transactions#summary'
