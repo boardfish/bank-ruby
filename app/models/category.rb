df437759db2c196b525b4897cc2fb7e20fc87185
Simon Fish <sfish@ukcloud.com>
2018-08-19 18:30:26 +0100

    Document category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Category: Group of purchases under a particular name. Represents a user-
+# assigned budget.
 class Category < ApplicationRecord
   before_validation :snakecase_name
   validates_uniqueness_of :name, case_sensitive: false
