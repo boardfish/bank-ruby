8df98b17c5eec221b0e17b90af7ddf1b8e792cd0
Simon Fish <sfish@ukcloud.com>
2018-08-19 12:49:17 +0100

    Snakecase category name before validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ApplicationRecord
+  before_validation :snakecase_name
   validates_uniqueness_of :name, case_sensitive: false
   validates_presence_of :name
+
+  def snakecase_name
+    self.name = name.parameterize.underscore
+  end
 end
