eb4c8941ffb69f35499d802b4dc0b1b12e62cb94
Simon Fish <sfish@ukcloud.com>
2018-08-21 15:35:49 +0100

    Fix: enable dynamic attributes for factories

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -2,6 +2,6 @@
 
 FactoryBot.define do
   factory :category do
-    name 'MyString'
+    name { 'MyString' }
   end
 end
--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -2,13 +2,13 @@
 
 FactoryBot.define do
   factory :merchant do
-    name 'MyString'
-    logo 'MyString'
-    address 'MyString'
-    monzo_id 'MyString'
-    group_id 'MyString'
-    created '2018-08-20 13:03:10'
-    created_at '2018-08-20 13:03:10'
-    updated_at '2018-08-20 13:03:10'
+    name { 'MyString' }
+    logo { 'MyString' }
+    address { 'MyString' }
+    monzo_id { 'MyString' }
+    group_id { 'MyString' }
+    created { '2018-08-20 13:03:10' }
+    created_at { '2018-08-20 13:03:10' }
+    updated_at { '2018-08-20 13:03:10' }
   end
 end
