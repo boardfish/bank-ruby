fdc9b9e34b83300f3aef5572b3910a33060a63a0
Simon Fish <sfish@ukcloud.com>
2018-08-21 15:52:27 +0100

    Create transaction uniqueness test, open others

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,5 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'must have a unique Monzo ID' do
+    FactoryBot.build(:transaction).save
+    duplicate_transaction = FactoryBot.build(:transaction)
+    duplicate_transaction.valid?
+    expect(duplicate_transaction.errors[:monzo_id])
+      .to include('has already been taken')
+  end
+
+  pending 'has all given info when created by a request to monzo_webhook_add' do
+  end
+
+  pending 'has all given info when created via seeding' do
+  end
 end
