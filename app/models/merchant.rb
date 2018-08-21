1c48ba253ce6fa74ab0c9b34b24cc425e6c9b38a
Simon Fish <sfish@ukcloud.com>
2018-08-21 15:51:03 +0100

    Add tests for, and fix, merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
+# Merchant: Represents the second party in a Monzo transaction.
 class Merchant < ApplicationRecord
+  validates_presence_of :monzo_id
+  validates_uniqueness_of :monzo_id
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -3,5 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should have a unique Monzo ID' do
+    FactoryBot.build(:merchant).save
+    duplicate_merchant = FactoryBot.build(:merchant)
+    duplicate_merchant.valid?
+    expect(duplicate_merchant.errors[:monzo_id])
+      .to include('has already been taken')
+  end
 end
