37d208912dd051104fb02083aa76719f8ddc7997
Simon Fish <si@mon.fish>
2018-08-26 11:41:39 +0000

    Add create and list options for categories

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'transactions#index'
   resources :transactions
+  resources :categories
   post '/monzo_webhook_add', to: 'transactions#monzo_webhook_add'
   get '/summary', to: 'transactions#summary'
 end
