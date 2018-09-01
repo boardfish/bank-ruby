8208dbc29f719af24aea81239a41380b176a1144
Simon Fish <sfish@ukcloud.com>
2018-09-01 12:47:05 +0100

    Unassign category on destroy

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,10 +4,15 @@
 # assigned budget.
 class Category < ApplicationRecord
   before_validation :snakecase_name
+  before_destroy :unassign_related_transactions
   validates_uniqueness_of :name, case_sensitive: false
   validates_presence_of :name
 
   def snakecase_name
     self.name = name.parameterize.underscore
+  end
+
+  def unassign_related_transactions
+    Transaction.where(category_id: id).update_all(category_id: nil)
   end
 end
